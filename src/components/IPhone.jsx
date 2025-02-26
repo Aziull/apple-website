644f1754a573b1924da1ffb75df5f4150b3ba203
Aziul <y.nazar9@gmail.com>
2025-02-26 14:26:57 +0200

    fix errors

--- src/components/IPhone.jsx
+++ src/components/IPhone.jsx
@@ -6,11 +6,11 @@ Source: https://sketchfab.com/3d-models/apple-iphone-15-pro-max-black-df17520841
 Title: Apple iPhone 15 Pro Max Black
 */
 
-import React, { useEffect, useRef } from 'react'
-import { useGLTF, useTexture } from '@react-three/drei'
-import * as THREE from 'three'
+import { useGLTF, useTexture } from '@react-three/drei';
+import React, { useEffect } from 'react';
+import * as THREE from 'three';
 export function IPhone(props) {
-    const { nodes, materials } = useGLTF('/models/scene.glb')
+    const { nodes, materials } = useGLTF('apple-website/models/scene.glb')
 
     const texture = useTexture(props.item.img);
 
